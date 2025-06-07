e947888590487a2c4a96efd82cf84301f597fa1f
Yashaswi <yashaswi1423@gmail.com>
2025-06-07 14:56:18 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Navigation from '../public/images/components/navigation';
+import Hero from '../public/images/components/hero';
+const App = () => {
+  return (
+  <div>
+    <Navigation/>
+    <Hero/>
+  </div>
+  );
+};
+export default App
